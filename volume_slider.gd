b4ecf46ede52d0310eaacb8f1d50e8ef7e4c2fdb
gabe-clark <gabeclark.dev@pm.me>
2024-06-23 12:35:56 -0800

    refactor: Address linter errors

--- volume_slider.gd
+++ volume_slider.gd
@@ -11,9 +11,10 @@ func _ready():
 	value = db_to_linear(
 		AudioServer.get_bus_volume_db(bus_index)
 	)
-	
+
 func _on_value_changed(value: float):
 	AudioServer.set_bus_volume_db(
 		bus_index,
 		linear_to_db(value)
 	)
+
