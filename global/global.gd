eee93612a3ff24ebe0c7834bfc44f65e42000346
gabe-clark <gabeclark.dev@pm.me>
2024-07-12 14:54:40 -0800

    feat: Added save/load custom user config

--- global/global.gd
+++ global/global.gd
@@ -1,4 +1,17 @@
 extends Node
 
 var score := 0
+const CONFIG_FILE_PATH = "user://config.cfg"
 
+func save_custom_config(section: String, key: String, value, save_path = CONFIG_FILE_PATH):
+	# This function takes the current configuration, rewrites it back to the file, then overwrites
+	# once with the final call to config.set_value. I've implemented it this way because as of godot
+	# 4.2 there seems not to be an easy to append to a configuration file. I may have just 
+	# overlooked the documentation for that though. 
+	var config = ConfigFile.new()
+	var err = config.load("user://config.cfg")
+	for sec in config.get_sections():
+		for k in config.get_section_keys(sec):
+			config.set_value(sec, k, config.get_value(sec, k))
+	config.set_value(section, key, value)
+	config.save(save_path)
--- scenes/settings_menu.gd
+++ scenes/settings_menu.gd
@@ -3,10 +3,23 @@ extends Control
 @onready var resolutions_option_button = $MarginContainer/HBoxContainer/VBoxContainer/OptionButton
 
 func _ready():
-	print("Adding resolutions")
 	add_resolutions()
-	print("Added resolutions")
+	
+	# Load custom config
+	var config = ConfigFile.new()
+	var err = config.load(Global.CONFIG_FILE_PATH)
 
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return
+
+	for player in config.get_sections():
+		if config.has_section_key(player, "ResolutionString"):
+			var resolution_string = config.get_value(player, "ResolutionString")
+			get_window().set_size(GUI.resolutions[resolution_string])
+			GUI.center_window()
+			resolutions_option_button.selected = GUI.resolutions.keys().find(resolution_string)
+		
 func add_resolutions():
 	for r in GUI.resolutions:
 		resolutions_option_button.add_item(r)
@@ -20,3 +33,5 @@ func _on_option_button_item_selected(index):
 	var key = resolutions_option_button.get_item_text(index)
 	get_window().set_size(GUI.resolutions[key])
 	GUI.center_window()
+	Global.save_custom_config("DefaultPlayer", "ResolutionString", key)
+
